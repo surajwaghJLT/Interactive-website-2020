31e99aad4f927b6c38456dfd6f5779b2f6ff240c
nayJlt <nay.htet-aung@marsh.com>
2021-01-08 13:07:45 +0800

    fixes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-function initMap() {
-    mapA = new google.maps.Map(document.getElementById('g-map'), {
-        zoom: 16,
-        center: new google.maps.LatLng(1.2783134, 103.8509185),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    marker = new google.maps.Marker({
-        map: mapA,
-        position: new google.maps.LatLng(1.2783134, 103.8509185)
-    });
-}
-
 function displaySpecialityContent($source) {
     if ($(".specialty__item.is-active").length) {
         if ($source) {
@@ -66,17 +54,6 @@ $(function () {
         }
     });
 
-    var swiper = new Swiper('.swiper-container', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-
     displaySpecialityContent();
 
     $(".specialty__box").on("click", function() {
@@ -96,5 +73,16 @@ $(function () {
 
     $(window).resize(function() {
         displaySpecialityContent();
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
 });
